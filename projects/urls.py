e9f1b516b9caca9d133908784a624537c04ec71c
Patrick Taylor <ptrcktylr@icloud.com>
2021-03-14 13:39:28 -0400

    fixed nav, removed unused html files, created project detail view, project list view, update dashboard

--- projects/urls.py
+++ projects/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('projects/', views.projects, name='projects'),
+    path('projects/', views.ProjectListView.as_view(), name='projects'),
     path('profile/', views.profile, name='profile'),
+    path('project/<int:pk>/', views.ProjectDetailView.as_view(), name='project-detail'),
+    path('project/new/', views.ProjectCreateView.as_view(), name='project-create'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Project
+from tickets.models import Ticket
+from django.contrib.auth.models import User
 
 def dashboard(request):
     return render(request, 'projects/dashboard.html')
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    context = {
+        'projects': Project.objects.all(),
+        }
+    return render(request, 'projects/projects.html', context)
 
 def profile(request):
     return render(request, 'projects/profile.html')
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'projects/projects.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'projects'
+    ordering = ['-created_at']
+
+class ProjectDetailView(DetailView):
+    model = Project
+
+class ProjectCreateView(CreateView):
+    model = Project
+    fields = ['name', 'description', 'assigned_users']
